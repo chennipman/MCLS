4f635caf877b6da94b248de766c013869f1fdaad
Coen <chennipman@hotmail.com>
2014-08-12 21:22:05 +0200

    Added a file with all the offical TU-colors.

---
+++ scripts/TUcolors.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 10 22:42:37 2014
+
+@author: coen
+
+The color of the TU Delft
+"""
+lavender  = '#7BA0C9' 
+fuchsia   = '#A10058'
+orange    = '#EC7F2C'
+yelgreen  = '#ADC610'
+hotpurple = '#83267C'
+yellow    = '#F7EB90' 
+
+TUcolors = [lavender, fuchsia, orange, yelgreen, hotpurple, yellow]
